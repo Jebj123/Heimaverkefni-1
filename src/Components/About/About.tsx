f30e0d8a8233acf4997f39d49008cfa6a73b6a84
Doird <90353914+Doird@users.noreply.github.com>
2026-02-24 10:04:31 +0000

    input+buttons

---
+++ src/Components/About/About.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import goodDog from "/src/assets/goodDog.png"
+import "./AboutStyle.css"
+
+const about = () => {
+  return (
+    <div className='containerAbout'>
+        <div className='wrapperAbout'>
+            <div>
+                <img className='goodDog' src={goodDog}></img>
+            </div>
+            <div className='chatBubble1'>
+                <h1 className='h2'>Um Okkur</h1>
+                <h2 className='paraAbout'>Erum að safna undirskriftum til að auka lífsgæði Kronos</h2>
+            </div>
+            
+            <div className='chatBubble2'>
+                <h2 className='h2'>Hvað Viljum við?</h2>
+                <p className='paraAbout2'> Við  viljum  að hver undirskrift er samansem og eitt klapp, eitt klór, ein dolla af hans uppáhalds blautmat og eitt leikfang í þokkabót</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default about
